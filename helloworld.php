0e05032e07901b40fc7c79ac2ad93d1ef564834a
Jelle bergsma <97062543@st.deltion.nl>
2021-11-09 20:05:29 +0100

    Aaanpassing opdracht 4.2

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,13 @@
 <?php
+#taak schrijf jouw eigen helloworld programma
 echo "Hello World \n";
+#4.1 eerste opdracht een keer echo
+#het printen en daarna een nieuw lijn
+#4.2
+echo "hello world \n";
+echo "hello world \n";
+echo "hello world \n";
+echo "hello world \n";
+#ik verwacht dat het 4x word laten zien en daarna nieuwe lijnen
+dus meerdere hello world.
 ?>
