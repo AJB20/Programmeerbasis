a9b198602bf91e6160c64ba90651bc04b6bce444
Jelle bergsma <97062543@st.deltion.nl>
2021-11-09 20:20:03 +0100

    Verandering door ! aan hello world
    hello world verandert door er ! aan toegevoegd

--- helloworld.php
+++ helloworld.php
@@ -4,9 +4,9 @@ echo "Hello World \n";
 #4.1 eerste opdracht een keer echo
 #het printen en daarna een nieuw lijn
 #4.2
-echo "hello world \n";
-echo "hello world \n";
-echo "hello world \n";
-echo "hello world \n";
+echo "hello world! \n";
+echo "hello world! \n";
+echo "hello world! \n";
+echo "hello world! \n";
 #ik verwacht dat het 4x word laten zien.
 ?>
