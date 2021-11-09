bb7bc02a6ce1984f6f2fda3070e0a425fc073ed3
Jelle bergsma <97062543@st.deltion.nl>
2021-11-09 20:22:57 +0100

    variable
    variable aangemaakt genaamd tekst en dit met echo laten zien

--- helloworld.php
+++ helloworld.php
@@ -1,12 +1,9 @@
 <?php
-#taak schrijf jouw eigen helloworld programma
-echo "Hello World \n";
-#4.1 eerste opdracht een keer echo
-#het printen en daarna een nieuw lijn
-#4.2
-echo "hello world! \n";
-echo "hello world! \n";
-echo "hello world! \n";
-echo "hello world! \n";
-#ik verwacht dat het 4x word laten zien.
+$tekst="Hello World!\n";
+
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
 ?>
