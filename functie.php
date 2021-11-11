02ebc1105fddfc1028befad9e9d018fd3e7848ce
Jelle Bergsma <97062543@st.deltion.nl>
2021-11-11 12:06:37 +0100

    Functie aangemaakt en getest uitleg volgt nog

--- functie.php
+++ functie.php
@@ -3,9 +3,15 @@ function func() {
     echo "+deze regel is in een func() uitgevoerd.\n";
 
 }
-echo "+deze regel is voor een func() uitgevoerd.\n";
+echo "+deze regel is voor een for-lus uitgevoerd.\n";
 
-func();
-echo "== Deze regel is na een func() uitgeoverd.\n";
+for ($counter=1;$counter<=3;$counter++) {
+    echo "=deze code is in de forlus uitgevoer\n";
+    echo "maar voor de func() uitgevoerd";
+    func();
+    echo "=deze code is in de for lus uitgevoerd";
+    echo " maar na func() uitgevoerd.\n";
+    echo "- Deze code is na de for-lus uitgevoerd.\n";
 
+}
 ?>
