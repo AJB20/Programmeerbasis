e397794094d63c93a34e8aaacacc50fd7a721ceb
Jelle Bergsma <97062543@st.deltion.nl>
2021-11-11 11:27:58 +0100

    functie opdrachth 11.2

---
+++ functie.php
@@ -0,0 +1,11 @@
+<?php
+function func() {
+    echo "+deze regel is in een func() uitgevoerd.\n";
+
+}
+echo "+deze regel is voor een func() uitgevoerd.\n";
+
+func();
+echo "== Deze regel is na een func() uitgeoverd.\n";
+
+?>
