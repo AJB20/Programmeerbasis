f038db8fbd2dcd53b9017eb63cb7fc9254ad8f75
Jelle Bergsma <97062543@st.deltion.nl>
2021-11-11 13:23:44 +0100

    array
    eerste array aangemaakt

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+#in dit php bestand ga ik verschillende arrays proberen.
+
+#array 1 
+#array dat de inhoud vrijdag,zaterdag,zondag heeft.
+$weekenddagen= array("vrijdag","zaterdag","zondag");
+echo $weekenddagen[0]
+#met echo word de inhoud laten zien van de array maar om andere uitkomsten te krijgen moet je het nummer veranderen.
+
+
+?>
