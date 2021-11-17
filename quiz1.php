7dd3617accff4035d98b3772e8a48477e2273509
Jelle Bergsma <97062543@st.deltion.nl>
2021-11-17 13:47:09 +0100

    quiz1 geupdae ze documentatie
    werkt nog niet volledig maar zit wel in functie

--- quiz1.php
+++ quiz1.php
@@ -1,24 +1,29 @@
 <?php
-$random = rand(1,1);
+$random = rand(1,100);
 $raad= readline("raad het getal, voer getal in: \n");
 
-echo "$raad \n";
+
+print "$raad \n";
 
 
 
-if ($raad>100){
-    exit("getal kan niet groter zijn dan 100!");
-}
-if ($raad<1) {
-    exit("Getal kan niet lager zijn dan 1 !");
-}
+function quiz($raad,$random) {
+    if ($raad>100){
+        exit("getal kan niet groter zijn dan 100!");
+    }
+    if ($raad<1) {
+        exit("Getal kan niet lager zijn dan 1 !");
+    }
+    if ($raad ==$random) {
+        echo "juist\n"; 
+    }  
+     elseif ($raad >$random) {
+        echo"getal te hoog probeer opnieuw\n";
+        readline("raad het getal, voer getal in: \n");
+     }
+        echo"getal te laag\n"; {
+        readline("raad het getal, voer getal in: \n");
+    }
+    }
 
-  if ($raad ==$random) {
-    echo "juist\n";   
-} elseif ($raad >$random) {
-    echo "getal te hoog probeer opnieuw\n";
-    readline("raad het getal, voer getal in: \n");
-} else {
-    echo "getal te laag\n";
-    readline("raad het getal, voer getal in: \n");
-}
+    quiz($raad,$random);
