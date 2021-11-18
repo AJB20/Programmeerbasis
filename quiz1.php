e23363166bc5eca4e9e26bc43950310615b63ace
Jelle Bergsma <97062543@st.deltion.nl>
2021-11-18 14:26:35 +0100

    WIP - eisen bijna voltooid alleen hoeveel tries nog
    eissen zijn groten deels voltooid tellen hoe vaak user heeft geprobeert te raden moet nog geschreven wroden.

--- quiz1.php
+++ quiz1.php
@@ -1,29 +1,64 @@
 <?php
-$random = rand(1,100);
-$raad= readline("raad het getal, voer getal in: \n");
+
+echo "Welkom bij getal raad quiz, kies een getal tussen de 1 en 100.\n";
+echo "Let op: het programma stopt bij een getal onder de 1 en boven de 100!\n";
+$random = rand(1,1);
+$vraag=readline("voer uw bedachte getal in: \n");
 
 
-print "$raad \n";
+stoppen_cijfer()&&stop_woord(); 
+goed_hoog_laag();
 
 
 
-function quiz($raad,$random) {
-    if ($raad>100){
-        exit("getal kan niet groter zijn dan 100!");
+//-----------------------------------------------------------------------------------
+
+//stoppen werkt?:
+function stoppen_cijfer() {
+    global $vraag;
+    if ($vraag<1) {
+
+        exit();
     }
-    if ($raad<1) {
-        exit("Getal kan niet lager zijn dan 1 !");
-    }
-    if ($raad ==$random) {
-        echo "juist\n"; 
-    }  
-     elseif ($raad >$random) {
-        echo"getal te hoog probeer opnieuw\n";
-        readline("raad het getal, voer getal in: \n");
+         elseif ($vraag>100) {
+             exit();
+         }
      }
-        echo"getal te laag\n"; {
-        readline("raad het getal, voer getal in: \n");
-    }
-    }
 
-    quiz($raad,$random);
+     function stop_woord() {
+        global $vraag;
+        global $stop;
+        if ($vraag=="stop") {
+            exit();
+        }
+         }
+        
+
+
+
+
+         function goed_hoog_laag() {
+             global $vraag;
+             global $random;         
+             if ($vraag==$random) {
+                 echo "geraden";
+                return;
+               global $opnieuw;
+         } elseif ($vraag>$random) {
+            echo "te hoog getal, probeer een lager getal\n";
+            $opnieuw= readline("uw nieuwe getal (lager):");   
+         } elseif ($vraag<$random) {
+            echo "te laag getal, probeer een hoger getal\n";
+            $opnieuwh= readline("uw nieuwe getal (lager):");  
+            $opnieuwh; 
+         }
+            
+         }
+         
+         
+ 
+count()
+        
+        
+
+?>
