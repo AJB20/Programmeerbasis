9b232887b5a9395b4eb78268017e2c986f8375a8
Jelle Bergsma <97062543@st.deltion.nl>
2021-11-23 10:22:22 +0100

    kortere zinnen en ongebruikte variable weg
    Kortere zinnen zodat het sneller te begrijpen is door de gebruiker + ongebruikte variabelen weg gehaald.

--- quiz.php
+++ quiz.php
@@ -1,49 +1,50 @@
 <?php
 
-$aantalx_raden = 0;
-$goed_geraden = 0;
+echo "Welkom, raad het getal.\n";
+echo "getal onder 1 dan stopt het programma\n";
+echo "getal boven 100 dan stopt het programma\n";
+echo "stoppen? type: stop.\n";
 
 
+function opnieuw() {
 
-echo "elkom, in dit programma is het de bedoeling dat je het getal raad wat het programma heeft bedacht.\n";
-echo "Het getal mag niet onder de 1 worden ingevoerd dan stopt het programma.\n";
-echo "Het getal mag ook niet boven de 100 zijn dan stopt het programma.\n";
-echo "Wil je het programma zelf stoppen typ dan: stop.\n";
-echo "Succes!\n"; 
+
+}
+
+
 
 function getal_raden() {
-global $aantalx_raden, $goed_geraden;
+$aantalx_raden = 0;
 
 $aantalx_raden ++;
 $programma_nummer= rand(1,2);
-echo "typ hier je getal";
-$spelernummer= readline(">>");
+$spelernummer= readline("uw getal: \n");
 $spelernummer= intval($spelernummer);
 
 if ($spelernummer=="stop") {
     exit("gestopt");
 }
 if ($spelernummer<0) {
-    echo "lager dan nul programma stopt";
-    exit();
+    exit("gestopt");
 }
 if ($spelernummer>100) {
-    echo "hoger dan 100 programma stopt.";
-    exit();
+    exit("gestopt");
 }
     if ($spelernummer==$programma_nummer) {
         $aantalx_raden;
-        echo "goed geraden!\n";
-        echo "aantal keer geprobeert: $aantalx_raden";
+        echo "goed\n";
+        echo "keren: $aantalx_raden";
         exit();
     }
     elseif ($spelernummer>$programma_nummer) {
         $aantalx_raden++;
+        echo "te laag";
         getal_raden();
         exit();
     }
     elseif ($spelernummer<$programma_nummer) {
         $aantalx_raden++;
+        echo "te hoog";
         getal_raden();
         exit();
         
