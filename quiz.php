cbe24e0be2e83bf7dcd383909cbc6054d7ae62ce
Jelle Bergsma <97062543@st.deltion.nl>
2021-11-23 11:16:59 +0100

    die in plaats van exit, tekst aangepast
    zelfde functionaliteit maar is duidelijker.

--- quiz.php
+++ quiz.php
@@ -1,61 +1,42 @@
 <?php
 
 echo "Welkom, raad het getal.\n";
-echo "getal onder 1 dan stopt het programma\n";
-echo "getal boven 100 dan stopt het programma\n";
+echo "is uw getal onder de 1 dan stopt het programma\n";
+echo "is uw getal boven de 100 dan stopt het programma\n";
 echo "stoppen? type: stop.\n";
 
-
-function opnieuw() {
-
-
-}
-
-
-
 function getal_raden() {
-$aantalx_raden = 0;
-
-$aantalx_raden ++;
-$programma_nummer= rand(1,2);
+$programma_nummer= rand(1,100);
 $spelernummer= readline("uw getal: \n");
 $spelernummer= intval($spelernummer);
-
+$aantalx_raden = 0;
 if ($spelernummer=="stop") {
-    exit("gestopt");
+    die("gestopt");
 }
-if ($spelernummer<0) {
-    exit("gestopt");
+if ($spelernummer<1) {
+    die("gestopt");
 }
 if ($spelernummer>100) {
-    exit("gestopt");
+    die("gestopt");
 }
     if ($spelernummer==$programma_nummer) {
-        $aantalx_raden;
+        $aantalx_raden++;
         echo "goed\n";
         echo "keren: $aantalx_raden";
-        exit();
+        die();
     }
     elseif ($spelernummer>$programma_nummer) {
         $aantalx_raden++;
-        echo "te laag";
+        echo "te hoog";
         getal_raden();
-        exit();
+        die();
     }
     elseif ($spelernummer<$programma_nummer) {
         $aantalx_raden++;
-        echo "te hoog";
+        echo "te laag";
         getal_raden();
-        exit();
-        
-       
+        die();               
     }
 }
-
-
-getal_raden();
-
-
-
 
 ?>
