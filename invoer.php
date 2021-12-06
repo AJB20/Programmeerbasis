febaa4c17ff272d466e2789846376f06175d141f
Jelle Bergsma <97062543@st.deltion.nl>
2021-12-06 13:23:36 +0100

    aanpassingen gedaan nog niet werkent
    programma werkt nog niet naar behoren

--- invoer.php
+++ invoer.php
@@ -1,15 +1,16 @@
-Lisng 9.2: tafel.php
- <?php
 
+ <?php
+//code onderzoeken want ik begrijp het nog niet.
  $tafel=readline("Welke tafel wil je zien? ");
  $begin=1;
  $eind=10;
+ $teller=$begin;$teller<=$eind;$teller++;
+ echo "Tafel van $tafel:\n";
 
- echo "\n\nTafel van $tafel:\n";
-
- for ($teller=$begin;$teller<=$eind;$teller++) {
+ for($teller=$begin;$teller<=$eind;$teller++); {
  $product=$teller*$tafel;
- echo $teller." x ".$tafel." = "$product."\n";
+ echo "$teller."keer".$tafel." = $product."\n";
  }
+}
 
  ?>
