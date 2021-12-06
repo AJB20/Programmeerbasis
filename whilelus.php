e340feac9b3a22ca7e93c4650fd5f477a5433d82
Jelle Bergsma <97062543@st.deltion.nl>
2021-12-06 13:24:26 +0100

    meer informatie over de werking
    meer informatie over de werking gedocumenteerd

--- whilelus.php
+++ whilelus.php
@@ -1,10 +1,13 @@
 <?php
-
- $teller=1;
+//veriabele teller heeft de waarde 1 
+ $teller=1; 
+ //variabele maximum heeft de waarde 10.
  $maximum=10;
-
+//hier word laten zien aan de gebruiker (laten zien op zijn of haar scherm) wat de waarde van $teller is voordat de while lus word uitgevoert
  echo "Teller is voor de while lus ".$teller."\n";
-
+//hier word de while lus uitgevoert, hier staat zo lang  de variabele teller kleiner of gelijk aan variabele maximum is laat de gebruiker zien wat na de echo staat en welk nummer in de variabele staat.
+// als de while lus is uitgevoert tel bij variabele teller 1 op.
+//
  while ( $teller <= $maximum ) {
      echo "Teller is nu ".$teller."\n";
  $teller++;
