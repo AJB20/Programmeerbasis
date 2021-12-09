219629537ec605152645c37f579a39e7c7ba8c14
Jelle Bergsma <jbb19@protonmail.com>
2021-12-09 10:07:48 +0000

    documentatie
    meer documentatie toegevoegt aan reken.php

--- Reken.php
+++ Reken.php
@@ -1,26 +1,26 @@
 <?php
-$getal1=10;
-$getal2=2;
+$getal1=10; //variabele met waarde 10
+$getal2=2; //variabele met waarde 2 
 
-$som=$getal1 + $getal2;
-echo "$getal1 + $getal2 = ".$som;
-echo "\n";
+$som=$getal1 + $getal2; //variabele som met een plus som van bovenstaande getallen
+echo "$getal1 + $getal2 = ".$som; //gebruiker word variabele getal1 en getal twee getoont en het resultaat
+echo "\n"; //nieuwe lege lijn word laten zien
 
-$verschil=$getal1 - $getal2;
-echo "$getal1 - $getal2 = ".$verschil;
-echo "\n";
+$verschil=$getal1 - $getal2; //nieuwe veriabele $verschil word aangemaakt met een aftel som
+echo "$getal1 - $getal2 = ".$verschil; //variabele geval1 en getal2 worden getoont en het resultaat word getoont van de aftel som
+echo "\n"; //nieuwe lege lijn word laten zien 
 
-$product=$getal1 * $getal2;
-echo "$getal1 * $getal2 = ".$product;
-echo "\n";
+$product=$getal1 * $getal2; //nieuwe variabele product met waardes getal keer getal2
+echo "$getal1 * $getal2 = ".$product; //getal1 en 2 worden getoont en de uitkomst van de keer som
+echo "\n"; //nieuwe lege lijn
 
-$quotient=$getal1 / $getal2;
-echo "$getal1 / $getal2 = ".$quotient;
-echo "\n";
+$quotient=$getal1 / $getal2; //nieuwe variabele quotinet/delen met waardes getal1  en  getal2
+echo "$getal1 / $getal2 = ".$quotient; //getal1 en 2 worden getoont en de uitkomst van de deel som
+echo "\n"; //nieuwe lege lijn
 
-$rest=$getal1 % $getal2;
-echo "$getal1 % $getal2 = ".$rest;
-echo "\n";
+$rest=$getal1 % $getal2;  //nieuwe variabele rest  met waardes getal1  en  getal2
+echo "$getal1 % $getal2 = ".$rest; //getal1 en 2 worden getoont en de uitkomst van de rest som
+echo "\n"; //nieuwe lege lijn
 ?>
 
 
