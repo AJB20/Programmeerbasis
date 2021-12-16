0bcbe19c5a1fbec921aedeb9ff45e457154b7f4e
Jelle Bergsma <jbb19@protonmail.com>
2021-12-16 10:51:01 +0000

    aanpassing meer overzichtelijk
    meer overzichterlijk gemaakt

--- quiz.php
+++ quiz.php
@@ -5,17 +5,21 @@
 #snum = spelernummer
 #ar = aantal keer raden
 #pnum= programma nummer
+#gr=functie getal raden 
+
+
 
 echo "Welkom,\n\n";
-echo "REGELS:\n\n getal onder de 1 niet toegestaan. \n\n getal boven de 100 niet toegestaan\n\n stoppen? typ: s of stop.\n\n\n\n";
+echo "Regels:\n Getal onder de 1 NIET toegestaan.\n getal boven de 100 NIET toegestaan.\n wilt u stoppen? typ: s of stop.\n\n";
+echo "worden de regels van dit programma overtreden stopt het programma!\n\n";
 $pnum= rand(1,100);
 echo $pnum;
 $ar =0;
-#functie genaamd getal_raden 
+#functie genaamd gr 
 #rand = random getal door programma gegenereeerd tussen de 1 en de 100
 #snum = speler word gevraagd om een getal en met intval word gekeken of het wel een getal is.
 #ar = hoe vaak user heeft geprobeert te raden.
-function getal_raden() {
+function gr() {
 global $pnum;
 global $ar;
 $snum= readline("\n\n\n\n uw getal: \n\n");
@@ -37,23 +41,24 @@ if ($snum>100) {
 }
 #als spelernummmer gelijk aan programma nummer is laat dan onderstaande zien en stop het programma
     if ($snum==$pnum) {
-        
+        $ar++;
         echo "goed\n\n";
         echo "goed geraden! keren: $ar";        
     }
     #als snum groter is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
     elseif ($snum>$pnum) {
         echo("TE HOOG\n\n");
-        getal_raden($pnum,$ar++);
+        gr($pnum,$ar++);
     }
     #als snum lager is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
     
     elseif ($snum<$pnum) {
-        echo("TE LAAG\n\n");
-        getal_raden($pnum,$ar++);           
+        echo("TE LAAG\n\n ");
+        echo("\u{U+1F613}")
+        gr($pnum,$ar++);  //hier word de functie getal raden aangeroepen en parameters pnum en ar          
     }  
 }
 
-getal_raden(); //funtie word uitgevoert.
+gr(); //funtie word uitgevoert.
 
 ?>
