6dcabf246cd52ae6e92daab44a24dcba72f904aa
Jelle Bergsma <jbb19@protonmail.com>
2021-12-16 10:05:05 +0000

    werkend maar nog niet tevreden
    alles werkt counter + het programma zelf kijken waar het nog verbetert kan worden

--- quiz.php
+++ quiz.php
@@ -1,59 +1,54 @@
 <?php #begin php
 #gemaakt door: arcticjelle (ikbenjelle.nl)
 #gebruiker krijg onderstaande zinnen te zien
-echo "Welkom, raad het getal.\n\n";
+#legenda afkortingen:
+#snum = spelernummer
+#ar = aantal keer raden
+#pnum= programma nummer
+
+echo "Welkom,\n\n";
 echo "is uw getal onder de 1 dan stopt het programma\n is uw getal boven de 100 dan stopt het programma\n\n stoppen? type: stop.\n";
-$programma_nummer= rand(1,100);
-echo $programma_nummer;
+$pnum= rand(1,100);
+echo $pnum;
+$ar =0;
 #functie genaamd getal_raden 
 #rand = random getal door programma gegenereeerd tussen de 1 en de 100
-#spelernummer = speler word gevraagd om een getal en met intval word gekeken of het wel een getal is.
-#aantalx_raden = hoe vaak user heeft geprobeert te raden.
+#snum = speler word gevraagd om een getal en met intval word gekeken of het wel een getal is.
+#ar = hoe vaak user heeft geprobeert te raden.
 function getal_raden() {
-global $programma_nummer;
-$spelernummer= readline("uw getal: \n\n");
-$aantalx_raden =0;
-$aantalx_raden++;
-$spelernummer= intval($spelernummer);
+global $pnum;
+global $ar;
+$snum= readline("uw getal: \n\n");
+$snum= intval($snum);
 
 
-#if spelernummer = stop betekent dat als spelernummer gelijk is aan het woord  stop dan stopt het programma.
-if ($spelernummer=="stop") {
-    die("gestopt");
-}
- 
+#if snum = stop betekent dat als snum gelijk is aan het woord  stop dan stopt het programma.
+if ($snum=="stop") {
+    die("gestopt"); 
 #ingevoerd getal kleiner dan 1 stop het programma
-if ($spelernummer<1) {
-    die("gestopt getal  mag niet lager zijn dan 1\n herstart het programma en probeer opnieuw");
+if ($snum<1) {
+    die();
 }
 #ingevoerd getal boven de 100 stop het programma
-if ($spelernummer>100) {
-    die("gestopt getal mag niet hoger zijn dan 100\n herstart het programma en probeer opnieuw");
+if ($snum>100) {
+    die();
 }
 #als spelernummmer gelijk aan programma nummer is laat dan onderstaande zien en stop het programma
-    if ($spelernummer==$programma_nummer) {
-        $aantalx_raden++;
+    if ($snum==$pnum) {
         
         echo "goed\n\n";
-        echo "goed geraden! keren: $aantalx_raden";
-        
+        echo "goed geraden! keren: $ar";        
     }
-    
-
-    #als spelernummer groter is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
-    elseif ($spelernummer>$programma_nummer) {
+    #als snum groter is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
+    elseif ($snum>$pnum) {
         echo("TE HOOG\n\n");
-        $aantalx_raden++;
-        getal_raden($programma_nummer);
-      
+        getal_raden($pnum,$ar++);
     }
-    #als spelernummer lager is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
+    #als snum lager is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
     
-    elseif ($spelernummer<$programma_nummer) {
-        echo("TE LAAG :\n\n");
-        getal_raden($programma_nummer);
-            
-            
+    elseif ($snum<$pnum) {
+        echo("TE LAAG\n\n");
+        getal_raden($pnum,$ar++);           
     }  
 }
 
