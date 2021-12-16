7198057daf855e99143953d11cbdaa2db16d1305
Jelle Bergsma <jbb19@protonmail.com>
2021-12-16 09:48:23 +0000

    while lus werkend en quiz.php zonder while lus werkt
    quiz.php werkt zonder while lus teller aanpassen.

--- quiz.php
+++ quiz.php
@@ -1,55 +1,60 @@
 <?php #begin php
-#gemaakt door: Jb200126 (ikbenjelle.nl)
-
-
-
+#gemaakt door: arcticjelle (ikbenjelle.nl)
 #gebruiker krijg onderstaande zinnen te zien
-echo "Welkom, raad het getal.\n";
-echo "is uw getal onder de 1 dan stopt het programma\n is uw getal boven de 100 dan stopt het programma\n stoppen? type: stop.\n";
-
-
+echo "Welkom, raad het getal.\n\n";
+echo "is uw getal onder de 1 dan stopt het programma\n is uw getal boven de 100 dan stopt het programma\n\n stoppen? type: stop.\n";
+$programma_nummer= rand(1,100);
+echo $programma_nummer;
 #functie genaamd getal_raden 
 #rand = random getal door programma gegenereeerd tussen de 1 en de 100
 #spelernummer = speler word gevraagd om een getal en met intval word gekeken of het wel een getal is.
 #aantalx_raden = hoe vaak user heeft geprobeert te raden.
 function getal_raden() {
-$programma_nummer= rand(1,100);
-$spelernummer= readline("uw getal: \n");
+global $programma_nummer;
+$spelernummer= readline("uw getal: \n\n");
+$aantalx_raden =0;
+$aantalx_raden++;
 $spelernummer= intval($spelernummer);
-$aantalx_raden = 0;
-#if spelernummer = stop betekent dat als spelernummer gelijk is aan stop dan stopt het programma.
+
+
+#if spelernummer = stop betekent dat als spelernummer gelijk is aan het woord  stop dan stopt het programma.
 if ($spelernummer=="stop") {
     die("gestopt");
 }
-#ingevoerd getal kleinder dan 1 stop het programma
+ 
+#ingevoerd getal kleiner dan 1 stop het programma
 if ($spelernummer<1) {
-    die("gestopt");
+    die("gestopt getal  mag niet lager zijn dan 1\n herstart het programma en probeer opnieuw");
 }
 #ingevoerd getal boven de 100 stop het programma
 if ($spelernummer>100) {
-    die("gestopt");
+    die("gestopt getal mag niet hoger zijn dan 100\n herstart het programma en probeer opnieuw");
 }
 #als spelernummmer gelijk aan programma nummer is laat dan onderstaande zien en stop het programma
     if ($spelernummer==$programma_nummer) {
         $aantalx_raden++;
-        echo "goed\n";
-        echo "keren: $aantalx_raden";
-        die();
+        
+        echo "goed\n\n";
+        echo "goed geraden! keren: $aantalx_raden";
+        
     }
+    
+
     #als spelernummer groter is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
     elseif ($spelernummer>$programma_nummer) {
+        echo("TE HOOG\n\n");
         $aantalx_raden++;
-        echo "te hoog";
-        getal_raden();
-        die();
+        getal_raden($programma_nummer);
+      
     }
     #als spelernummer lager is dan programma nummer is tel +1 pogingen er bij en laat te hoog zien geef user nog een kans en stop het programma
+    
     elseif ($spelernummer<$programma_nummer) {
-        $aantalx_raden++;
-        echo "te laag";
-        getal_raden();
-        die();               
-    }
+        echo("TE LAAG :\n\n");
+        getal_raden($programma_nummer);
+            
+            
+    }  
 }
 
 getal_raden(); //funtie word uitgevoert.
--- whilelus.php
+++ whilelus.php
@@ -7,7 +7,6 @@
  echo "Teller is voor de while lus ".$teller."\n";
 //hier word de while lus uitgevoert, hier staat zo lang  de variabele teller kleiner of gelijk aan variabele maximum is laat de gebruiker zien wat na de echo staat en welk nummer in de variabele staat.
 // als de while lus is uitgevoert tel bij variabele teller 1 op.
-//
  while ( $teller <= $maximum ) { //zo lang variabele $teller lager of gelijk aan variabele $maximum is voer de volgende code uit
      echo "Teller is nu ".$teller."\n"; //toon wat hier naast staat aan de gebruiker en het getal wat in de $teller variabele staat
  $teller++; //tel zo lang de variabele $teller gelijk of lager is dan variabele $maximum er +1 bij variabele $teller op
