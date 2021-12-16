7dcbde79ab8e61e9c53f0777e5d794b0c2688b68
Jelle Bergsma <jbb19@protonmail.com>
2021-12-16 10:51:28 +0000

    code update
    update van de code omdat er nog wat moest toegevoegt worden

--- quizclean.php
+++ quizclean.php
@@ -2,17 +2,16 @@
 
 
 echo "Welkom,\n\n";
-echo "REGELS:\n\n getal onder de 1 niet toegestaan. \n\n getal boven de 100 niet toegestaan\n\n stoppen? typ: s of stop.\n\n\n\n";
+echo "Regels:\n Getal onder de 1 NIET toegestaan.\n getal boven de 100 NIET toegestaan.\n wilt u stoppen? typ: s of stop.\n\n";
+echo "worden de regels van dit programma overtreden stopt het programma!\n\n";
 $pnum= rand(1,100);
 echo $pnum;
 $ar =0;
-
-function getal_raden() {
+function gr() {
 global $pnum;
 global $ar;
 $snum= readline("\n\n\n\n uw getal: \n\n");
 $snum= intval($snum);
-
 
 if ($snum=="stop"&&$snum=="s") {
     die("het programma stopt");
@@ -26,25 +25,24 @@ if ($snum<1) {
 if ($snum>100) {
     die();
 }
-
+#
     if ($snum==$pnum) {
-        
+        $ar++;
         echo "goed\n\n";
         echo "goed geraden! keren: $ar";        
     }
     
     elseif ($snum>$pnum) {
         echo("TE HOOG\n\n");
-        getal_raden($pnum,$ar++);
+        gr($pnum,$ar++);
     }
-  
+
     
     elseif ($snum<$pnum) {
-        echo("TE LAAG\n\n");
-        getal_raden($pnum,$ar++);       
+        echo("TE LAAG\n\n ");
+        gr($pnum,$ar++);     
     }  
 }
 
-getal_raden();
-
+gr();
 ?>
